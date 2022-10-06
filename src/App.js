f607e6f4c39bf5de18019bed1ffcfd8545bb5754
KelV1952 <vladimir.kelyarsky@gmail.com>
2022-10-06 18:23:50 +0300

    создан новый компонент PostFilter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './styles/App.css';
 import './components/PostItem';
 // import PostItem from './components/PostItem';
@@ -6,7 +6,8 @@ import PostList from './components/PostList';
 // import MyButton from './components/UI/button/MyButton.module.css';
 // import MyInput from './components/UI/input/MyInput.module.css';
 import PostForm from './components/PostForm';
-import MySelect from './components/UI/select/MySelect';
+// import MySelect from './components/UI/select/MySelect';
+import PostFilter from './components/PostFilter';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,40 +16,49 @@ function App() {
     { id: 3, title: 'fJavascript3', body: 'aDiscription' },
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('');
+  // const [selectedSort, setSelectedSort] = useState('');
+  // const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState({
+    query: '',
+    sort: '',
+  });
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortedPosts = useMemo(() => {
+    console.log('working function SortedPosts');
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-  };
-
-  const sortPosts = (sort) => {
-    console.log(sort);
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Сортировка "
-          options={[
-            { value: 'title', name: 'по названию' },
-            { value: 'body', name: 'по описанию' },
-          ]}
-        />
-      </div>
+      <PostFilter filter={filter} setFilter={setFilter} />
 
-      {posts.length !== 0 ? (
-        <PostList posts={posts} title="Список постов" remove={removePost} />
+      {sortedAndSearchedPosts.length !== 0 ? (
+        <PostList
+          posts={sortedAndSearchedPosts}
+          title="Список постов"
+          remove={removePost}
+        />
       ) : (
         <div>
           <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
