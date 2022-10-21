bebd4ee4727cba21c1fb34217a7119ee69ce50b0
KelV1952 <vladimir.kelyarsky@gmail.com>
2022-10-21 20:28:09 +0300

    исправление в PostList.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import './components/PostItem';
-// import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 // import MyInput from './components/UI/input/MyInput.module.css';
 import PostForm from './components/PostForm';
-// import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import { usePosts } from './hooks/usePosts';
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import PostItem from './PostItem';
-import '../styles/App.css';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const PostList = ({ posts, remove, title }) => {
-  // if (!posts.length) {
-  //   return <h2 style={{ textAlign: 'center' }}>Посты не найдены!</h2>;
-  // }
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>{title}</h2>
       <TransitionGroup>
         {posts.map((post, index) => (
-          <CSSTransition key={post.id} timeout={500} className="post">
+          <CSSTransition key={post.id} timeout={500} classNames="post">
             <PostItem post={post} number={index + 1} remove={remove} />
           </CSSTransition>
         ))}
