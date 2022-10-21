e693720308ad0049dcde0191f6e8c5904fc153b5
KelV1952 <vladimir.kelyarsky@gmail.com>
2022-10-21 19:19:31 +0300

    реализована анимация

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostForm from './components/PostForm';
 // import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -22,26 +23,11 @@ function App() {
     query: '',
     sort: '',
   });
+  const sortedAndSearchedPosts = usePosts(posts, filter.query, filter.sort);
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-
-  const sortedPosts = useMemo(() => {
-    console.log('working function SortedPosts');
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import PostItem from './PostItem';
+import '../styles/App.css';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const PostList = ({ posts, remove, title }) => {
+  // if (!posts.length) {
+  //   return <h2 style={{ textAlign: 'center' }}>Посты не найдены!</h2>;
+  // }
   return (
     <div>
-      <p>
-        <h2 style={{ textAlign: 'center' }}>{title}</h2>
-      </p>
-      {posts.map((post, index) => (
-        <PostItem
-          number={index + 1}
-          post={post}
-          key={post.id}
-          remove={remove}
-        />
-      ))}
+      <h2 style={{ textAlign: 'center' }}>{title}</h2>
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} timeout={500} className="post">
+            <PostItem post={post} number={index + 1} remove={remove} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
