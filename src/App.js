078397dcb126539c7781461abf1c14bc3e858d12
KelV1952 <vladimir.kelyarsky@gmail.com>
2022-10-22 21:14:40 +0300

    созданы два кастомных хука-usePosts, useSortedPosts

--- src/App.js
+++ src/App.js
@@ -15,13 +15,27 @@ function App() {
     { id: 2, title: 'bJavascript2', body: 'bDiscription' },
     { id: 3, title: 'fJavascript3', body: 'aDiscription' },
   ]);
-
   const [modal, setModal] = useState(false);
   const [filter, setFilter] = useState({
     query: '',
     sort: '',
   });
-  const sortedAndSearchedPosts = usePosts(posts, filter.query, filter.sort);
+
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  // const sortedPosts = useMemo(() => {
+  //   if (filter.sort) {
+  //     return [...posts].sort((a, b) =>
+  //       a[filter.sort].localeCompare(b[filter.sort])
+  //     );
+  //   }
+  //   return posts;
+  // }, [filter.sort, posts]);
+
+  // const sortedAndSearchedPosts = useMemo(() => {
+  //   return sortedPosts.filter((post) =>
+  //     post.title.toLowerCase().includes(filter.query.toLowerCase())
+  //   );
+  // }, [filter.query, sortedPosts]);
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
