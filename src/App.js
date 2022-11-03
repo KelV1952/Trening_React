ebd32eeeb10deb7c1e1332452c08acf4044bd8b8
KelV1952 <vladimir.kelyarsky@gmail.com>
2022-11-03 18:26:34 +0200

    добавлены стили на кнопки

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loder';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount } from './components/utils/pages';
+import { getPagesArray } from './components/utils/pages';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -19,13 +21,27 @@ function App() {
     query: '',
     sort: '',
   });
+
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers['x-total-count'];
+    setTotalPages(getPageCount(totalCount, limit));
   });
+
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage = (page) => {
+    setPage(page);
+    //fetchPosts();
   };
 
   useEffect(() => {
@@ -61,12 +77,17 @@ function App() {
           remove={removePost}
         />
       )}
-
-      {/* <PostList
-        posts={sortedAndSearchedPosts}
-        title="Список постов"
-        remove={removePost}
-      /> */}
+      <div className="page__wrapper">
+        {pagesArray.map((p) => (
+          <span
+            // onClick={changePage(p)}
+            key={p}
+            className={page === p ? 'page page__current' : 'page'}
+          >
+            {p}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
